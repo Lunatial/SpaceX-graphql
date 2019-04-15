ff6ede856612c744120c9d278a1d5423b60e22d1
Feleky Mirkó <feleky@gmail.com>
2019-04-16 00:36:36 +0200

    add react frontend

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 
 const schema = require("./schema.js");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.use(
   "/graphql",
